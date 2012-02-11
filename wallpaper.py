4334be1ef1d595dd3356f388e9a39adc32036667
qiangwang <wsxys08@gmail.com>
2012-02-11 14:16:18 +0800

    scratch

---
+++ wallpaper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from pprint import pprint
+
+#template
+
+
+#conf
+entrance = '/usr/share/gnome-background-properties/wall_paper.xml'
+context = 'context.xml'
+paths = ['/usr/share/backgrounds/*']
+formats = ['jpg','png']
+auto_only = True
+
+#generate
+import glob
+papers = []
+for path in paths:
+    for form in formats:
+        papers += glob.glob(path+'/*.'+form)
+#pprint(papers)
+
+import os
+context = {}
+for paper in papers:
+    names = os.path.split(paper)
+    fname = os.path.splitext(names[1])[0]
+    if not names[0] in context.keys():
+        context[names[0]] = []
+    context[names[0]].append({'name':fname,'filename':paper}) 
+#pprint(context)
