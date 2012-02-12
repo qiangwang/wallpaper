ff394c247d7d411c66aa236b01a964fa7120a91a
qiangwang <wsxys08@gmail.com>
2012-02-12 15:10:06 +0800

    use git

---
+++ conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+entrance = '/usr/share/gnome-background-properties/wall_paper.xml'
+doctype = ['wallpapers',None,'gnome-wp-list.dtd']
+auto_only = True
+
+paths = ['/usr/share/backgrounds/*']
+context = 'context.xml'
+formats = ['jpg','png']
+
