1d95c5df44e2592f1bcee240c52d1b82eff4378d
qiangwang <wsxys08@gmail.com>
2012-02-12 22:37:18 +0800

    readme update

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-entrance = '/usr/share/gnome-background-properties/wall_paper.xml'
+entrance = '/usr/share/gnome-background-properties/wallpapers.xml'
 doctype = ['wallpapers',None,'gnome-wp-list.dtd']
 static = True
 transition = True
