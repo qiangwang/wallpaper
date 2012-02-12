4e9b6fca9c1932c81369e63c25f57a10f0f82aac
qiangwang <wsxys08@gmail.com>
2012-02-12 13:57:55 +0800

    save

--- wallpaper.py
+++ wallpaper.py
@@ -2,34 +2,27 @@
 
 from pprint import pprint
 
-#template
-
-
-#conf
-entrance = '/usr/share/gnome-background-properties/wall_paper.xml'
-doctype = ['wallpapers',None,'gnome-wp-list.dtd'}
-auto_only = True
-
-paths = ['/usr/share/backgrounds/*']
-context = 'context.xml'
-formats = ['jpg','png']
+import conf
 
 #find wallpapers
 import glob
 papers = []
-for path in paths:
-    for form in formats:
+for path in conf.paths:
+    for form in conf.formats:
         papers += glob.glob(path+'/*.'+form)
 #pprint(papers)
 
 import os
-context = {}
+contexts = {}
 for paper in papers:
     names = os.path.split(paper)
     fname = os.path.splitext(names[1])[0]
-    if not names[0] in context.keys():
-        context[names[0]] = []
-    context[names[0]].append({'name':fname,'filename':paper}) 
-#pprint(context)
+    if not names[0] in contexts.keys():
+        contexts[names[0]] = []
+    contexts[names[0]].append({'name':fname,'filename':paper}) 
+pprint(contexts)
 
 #generate confs
+import dict_to_xml
+dict_to_xml.doctype = conf.doctype
+#for context in contexts.keys():
